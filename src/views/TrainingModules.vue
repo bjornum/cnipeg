145f112f257180c5ee1acc7213a0d2412fa0d1c9
bjornum <bjoern-einar2@hotmail.no>
2022-03-02 09:53:08 +0100

    Training Modules completed

--- src/views/TrainingModules.vue
+++ src/views/TrainingModules.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="homePageWidth">
     <v-row>
-      <v-col cols="12" class="mt-5"></v-col>
-      <v-col>Training Modules</v-col>
+      <v-col cols="12" class="mt-15"></v-col>
+      <v-col cols="2"></v-col>
+      <v-col cols="8">
+        <p class="trainingChapterTitle">Training Modules</p>
+        <p class="trainingChapterDescription">This page will be designed when the content is decided</p>
+      </v-col>
     </v-row>
   </div>
 </template>
@@ -23,6 +27,25 @@ export default {
 .homePageWidth {
   width: 90vw; 
   margin:auto auto;
+}
+
+.trainingChapterTitle {
+  font-family: 'Barlow', sans-serif;
+  font-weight: bold;
+  font-size: 46px;
+  color: #434343;
+  opacity: 1;
+  letter-spacing: 0px;
+  text-align: left;
+}
+.trainingChapterDescription {
+  font-family: 'Lato', sans-serif;
+  font-weight: normal;
+  font-size: 14px;
+  color: #6A6A6A;
+  opacity: 1;
+  letter-spacing: 0px;
+  text-align: left;
 }
 
 </style>
