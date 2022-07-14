1182f80a06316a7eca6aed0d40a184ac483b8dae
bjornum <bjoern-einar2@hotmail.no>
2022-07-14 12:07:08 +0200

    App.vue - Removed Ghost code * Slight formating the code

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <v-app>
     <Sidebar v-if="$vuetify.breakpoint.mdAndDown"></Sidebar>
     <Navbar v-else></Navbar>
+
     <v-main>
       <router-view/>
     </v-main>
@@ -12,8 +13,6 @@
 
 <script>
 import Navbar from "@/components/Global/Navbar.vue"
-import LanguageTest from "@/components/LanguageControll/LanguageTest.vue"
-import SelectLocale from "@/components/LanguageControll/SelectLocale.vue"
 import Sidebar from "@/components/Global/Sidebar.vue"
 import Footer from "@/components/Global/Footer.vue"
 
@@ -22,8 +21,6 @@ export default {
   components:{
     Navbar,
     Sidebar,
-    LanguageTest,
-    SelectLocale,
     Footer
   },
   data(){
@@ -35,13 +32,11 @@ export default {
 </script>
 
 <style>
+
   /* Global Project CSS */
   @import './assets/styles/GlobalStyling.css';
 
-
-  /* @import url('https://fonts.googleapis.com/css2?family=Barlow:wght@700&family=Lato:wght@400;700&display=swap'); */
-  /* @import url('https://fonts.googleapis.com/css2?family=Barlow:wght@700&family=Josefin+Sans&family=Lato:wght@400;700&display=swap'); */
-
-  /* Pros-sans 700 - carousel title  Heboo thin - description */
+  /* Pros-sans 700 - carousel title  Heboo thin - Fonts from googlefonts */
   @import url('https://fonts.googleapis.com/css2?family=Barlow:wght@700&family=Heebo:wght@100&family=Josefin+Sans&family=Lato:wght@400;700&family=Source+Sans+Pro:wght@700&display=swap');
+
 </style>
